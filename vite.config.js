e84480e25179e1b6508124160e3851c475dbade8
ramilsaavedra <ramilsmcl@gmail.com>
2024-07-10 16:13:40 +0800

    chore: hmr vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            clientPort: 5137,
+            host: 'localhost',
+            protocol: 'ws',
+
+        },
+        port: 5137,
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
