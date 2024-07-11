40f36575e0589dd9c31e7a2f552467768e12a792
ramilsaavedra <ramilsmcl@gmail.com>
2024-07-11 17:24:43 +0800

    feat: add softui font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         {{-- <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
